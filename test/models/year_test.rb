b5a5d2c10ec04b42b1f97a246841645549d381c0
melissa160 <melissa160@gmail.com>
2017-05-10 15:26:22 -0500

    Ingresando validaciones al modelo project para que pasen las pruebas

--- test/models/year_test.rb
+++ test/models/year_test.rb
@@ -4,4 +4,8 @@ class YearTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'should not save year without year' do
+    year = Year.new
+    assert_not project.save
+  end
 end
