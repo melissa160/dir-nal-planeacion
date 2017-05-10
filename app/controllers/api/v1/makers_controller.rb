0ea5245484261f8abc8406c5f5f79fad10a20eb4
melissa160 <melissa160@gmail.com>
2017-05-10 16:42:45 -0500

    implementando busqueda en la api de makers y projects

--- app/controllers/api/v1/makers_controller.rb
+++ app/controllers/api/v1/makers_controller.rb
@@ -1,5 +1,19 @@
 class Api::V1::MakersController < ApiController
   def index
     @makers = Maker.all
+    if params[:sector].present?
+      sector = params[:sector]
+      @makers = @makers.where("sector ILIKE ?", "%#{sector}%")
+    end
+    
+    if params[:unidad_ejecutora].present?
+      unidad_ejecutora = params[:unidad_ejecutora]
+      @makers = @makers.where("unidad_ejecutora ILIKE ?", "%#{unidad_ejecutora}%")
+    end
+
+    if params[:codigo].present?
+      codigo = params[:codigo]
+      @makers = @makers.where("codigo = ?", "#{codigo.to_i}")
+    end
   end
 end
--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -2,6 +2,9 @@ class Api::V1::ProjectsController < ApiController
   def index
     @maker = Maker.find(params[:maker_id])
     @projects = @maker.projects
+    if params[:nombre].present?
+      @projects = @projects.where("nombre ILIKE ?", "%#{params[:nombre]}%")
+    end
   end
 
 end
