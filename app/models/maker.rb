31e132c8f79e5a3b93e848411ae7540f11168d97
melissa160 <melissa160@gmail.com>
2017-05-10 15:12:50 -0500

    añadiendo validaciones al modelo maker para que pasen las pruebas unitarias

--- app/models/maker.rb
+++ app/models/maker.rb
@@ -1,6 +1,9 @@
 class Maker < ApplicationRecord
   require 'csv'
   has_many :projects
+  validates :unidad_ejecutora, presence: true
+  validates :codigo, presence: true
+  validates :sector, presence: true
 
 
   def self.import(file)
