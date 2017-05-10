1a09b157c66a37cacc22ae3e251a518cc8caebb6
melissa160 <melissa160@gmail.com>
2017-05-10 15:11:52 -0500

    añadiendo pruebas unitarias al modelo maker

--- app/models/maker.rb
+++ app/models/maker.rb
@@ -2,6 +2,7 @@ class Maker < ApplicationRecord
   require 'csv'
   has_many :projects
 
+
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
       Maker.create row.to_hash
--- test/models/maker_test.rb
+++ test/models/maker_test.rb
@@ -4,4 +4,19 @@ class MakerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test 'should not save maker without codigo' do
+    maker = Maker.new
+    assert_not maker.save
+  end
+
+  test 'should not save maker without sector' do
+    maker = Maker.new
+    assert_not maker.save
+  end
+
+  test 'should not save maker without unidad_ejecutora' do
+    maker = Maker.new
+    assert_not maker.save
+  end
 end
