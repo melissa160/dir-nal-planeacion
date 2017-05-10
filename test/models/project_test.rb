da547f7990483b68b09d2b6189e5d22068c97a91
melissa160 <melissa160@gmail.com>
2017-05-10 15:22:50 -0500

    escribiendo pruebas unitarias para el modelo project

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -4,4 +4,15 @@ class ProjectTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'should not save project without nombre' do
+    project = Project.new
+    assert_not project.save
+  end
+
+  test 'should not save project without year_id' do
+    project = Project.new
+    assert_not project.save
+  end
+
+
 end
