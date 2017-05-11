40ba30e2ff9109d6031117cb7bbaa4075792ce39
melissa160 <melissa160@gmail.com>
2017-05-11 15:14:40 -0500

    implementando metodo set_maker en controlador de projects

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::ProjectsController < ApiController
+  before_action :set_maker
+
   def index
-    @maker = Maker.find(params[:maker_id])
     @projects = @maker.projects
     if params[:nombre].present?
       @projects = @projects.where("nombre ILIKE ?", "%#{params[:nombre]}%")
     end
+  end
+
+  private
+  def set_maker
+    @maker = Maker.find(params[:maker_id])
   end
 
 end
